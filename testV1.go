43393aef23fbe996be41679aa74e1c918b3ea8c3
Alex <alex.nuka@yandex.ru>
2023-04-14 17:29:18 +0300

    Update testV1.go

--- testV1.go
+++ testV1.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("HELLO")
+	add := []float64{313, 3312, 14124, 13212, 4114, 24, 88}
+	a := summa(add)
+	rezult(a, add)
+	minimem(add)
+	maximal(add)
+
+}
+
+func summa(slis []float64) float64 {
+	var sum float64 = slis[0]
+	for _, val := range slis {
+		sum += val
+	}
+	fmt.Println("Сумма среза:", sum)
+	return sum
+}
+
+func rezult(sum float64, srez []float64) (r float64) {
+	r = float64(sum) / float64(len(srez))
+	fmt.Println("Среднее значение среза:", r)
+	return r
+}
+
+func minimem(add []float64) float64 {
+	var min float64 = add[0]
+	for _, val := range add {
+		if val < min {
+			min = val
+		}
+	}
+	fmt.Println("минимальное", min)
+	return min
+}
+
+func maximal(srez []float64) float64 {
+	var max float64 = srez[0]
+	for _, val := range srez {
+		if val > max {
+			max = val
+		}
+	}
+	fmt.Println("Максимальное число в срезе", max)
+	return max
 }
